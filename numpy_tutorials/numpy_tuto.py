61a770e1c18a0bb3a98029d9afb5d632d45a98fa
Nishal <75416251+darkracer3010@users.noreply.github.com>
2022-06-07 13:54:03 +0530

    Create numpy_tuto.py

---
+++ numpy_tutorials/numpy_tuto.py
@@ -0,0 +1,15 @@
+import numpy
+import numpy as np
+a=np.array([1,2,3])
+b=np.array([4,5,6])
+np.subtract(a,b)
+o=np.subtract(a,b)
+np.absolute(o)
+np.add(a,b)
+np.mod(a,b)
+n=np.array([1.2,5.6,4.3])
+np.ceil(n)
+np.around(n)
+g=np.array([1,2,3],dtype="S")
+b=np.array([[1,2,3],[4,5,6]])
+b.shape
